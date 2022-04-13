6a5ffa6a757527666e9210547ff5354d1817b23e
Emil Wagenplast <82289570+Peam269@users.noreply.github.com>
2022-04-13 19:22:16 +0200

    Initial commit

---
+++ release.sh
@@ -0,0 +1,46 @@
+ZIPPATH=out/target/product/raphael
+ZIPNAME=$(ls $ZIPPATH/dotOS-*.zip | tail -n1 | xargs -n1 basename)
+REPO=https://github.com/Peam269/dotos-builds
+
+METADATA=$(unzip -p "$ZIPPATH/$ZIPNAME" META-INF/com/android/metadata)
+TIMESTAMP=$(echo "$METADATA" | grep post-timestamp | cut -f2 -d '=')
+HASH=$(cut -f1 -d ' ' $ZIPPATH/$ZIPNAME.sha256sum)
+SIZE=$(du -b $ZIPPATH/$ZIPNAME | cut -f1 -d '	')
+VERSION=$(echo $ZIPPATH/$ZIPNAME | cut -f2 -d '-')
+DATE=$(echo $ZIPNAME | cut -f5 -d '-')
+DEVICE=$(echo "$METADATA" | grep pre-device | cut -f2 -d '=' | cut -f1 -d ',')
+RELEASENAME=${DEVICE}-${DATE}
+URL="$REPO/releases/download/${RELEASENAME}/${ZIPNAME}"
+
+
+echo "generatedAt": $TIMESTAMP,
+echo "fileName": "$ZIPNAME",
+echo "url": "$URL",
+echo "hash": "$HASH",
+echo "size": $SIZE,
+echo "version": "$VERSION"
+
+
+copy $ZIPPATH/dotOS-*.zip ~/android/builds/
+FILE="~/android/builds/dotos-builds/raphael.json"
+/bin/cat <<EOM >$FILE
+{
+  "codename": "raphael",
+  "deviceName": "Redmi K20 Pro",
+  "brandName": "Xiaomi",
+  "releases": [
+    {
+      "type": "UNOFFICIAL",
+      "generatedAt": $TIMESTAMP,
+      "fileName": "$ZIPNAME",
+      "url": "$URL",
+      "requireCleanFlash": false,
+      "images": null,
+      "hash": "$HASH",
+      "size": $SIZE,
+      "version": "$VERSION"
+    }
+  ]
+}
+EOM
+echo ready to release!
